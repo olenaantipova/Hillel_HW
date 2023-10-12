8a73fb4c67b60b8940b5fbb61ad3a26f949643f2
Olena Antipova <Olena.Antipova@playtech.com>
2023-10-12 20:46:29 +0300

    HW 9 - corrections

--- src/main/java/HW9/Task1_Recursion.java
+++ src/main/java/HW9/Task1_Recursion.java
@@ -3,17 +3,19 @@ package HW9;
 public class Task1_Recursion {
     public static void main(String[] args) {
         String s = "Java, World!";
-
-        if (s.length() > 0) {
-            recursion(s, s.length() - 1);
-        }
+        StringBuilder check = new StringBuilder(s);
+        StringBuilder s1 = new StringBuilder();
+        recursion(check, s1);
+        System.out.println(s1.toString());
     }
-    public static void recursion(String s, int index){
-        if(index == 0){
-            System.out.println(s.charAt(index));
+
+    public static void recursion(StringBuilder s, StringBuilder s1) {
+        if (s.length() == 0) {
             return;
         }
-        System.out.println(s.charAt(index));
-        recursion(s, index - 1);
+        s1.append(s.substring(s.length() - 1));
+        s.deleteCharAt(s.length() - 1);
+        recursion(s, s1);
+
     }
 }
--- src/main/java/HW9/Task3.java
+++ src/main/java/HW9/Task3.java
@@ -2,11 +2,22 @@ package HW9;
 
 public class Task3 {
     public static void main(String[] args) {
+        String initialString = "abcd4efabc123deabcdaaa";
+        String charactersToRemove = "ad3";
 
-        String str = "abcd4efabc123deabcdaaa";
-        str = str.replaceAll("[a]", "");
-        str = str.replaceAll("[d]", "");
-        str = str.replaceAll("[3]", "");
-        System.out.println(str);
+        String modifiedString = removeCharacters(initialString,charactersToRemove);
+        System.out.println("modified String " + modifiedString);
+    }
+    public static String removeCharacters (String init, String charactersToRemove){
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < init.length(); i++) {
+            char currentChar = init.charAt(i);
+
+            if (charactersToRemove.indexOf(currentChar) == -1) {
+                result.append(currentChar);
+            }
+        }
+        return result.toString();
     }
 }
